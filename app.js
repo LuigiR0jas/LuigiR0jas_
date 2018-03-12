736c4aeb2964ae0c909d038f4892f814bca09a21
Luigi Rojas <xluigix98@gmail.com>
2018-03-11 20:10:18 -0400

    initial code review and minimal changes

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+// Luigi: I will identify my own comments this way
+// Luigi: Also, I'll leave most if not all of Sam's comments. They're already as useful and funny as it gets.
+// TODO: Refactor code into jQuery
+
 import 'classlist-polyfill';
 import Promise from 'bluebird';
 import Markdown from 'markdown';
 const md = Markdown.markdown.toHTML;
 import workText from 'raw-loader!./work.txt';
 import pgpText from 'raw-loader!./pgp.txt';
-import headerHTML from 'raw-loader!./header.html';
+import footerHTML from 'raw-loader!./footer.html';
 let styleText = [0, 1, 2, 3].map(function(i) { return require('raw-loader!./styles' + i + '.css'); });
 import preStyles from 'raw-loader!./prestyles.css';
 import replaceURLs from './lib/replaceURLs';
@@ -18,14 +22,88 @@ let style, styleEl, workEl, pgpEl, skipAnimationEl, pauseEl;
 let animationSkipped = false, done = false, paused = false;
 let browserPrefix;
 
+// Helpers
+let endOfSentence = /[\.\?\!]\s$/;
+let comma = /\D[\,]\s$/;
+let endOfBlock = /[^\/]\n\n$/;
+
+
 // Wait for load to get started.
 document.addEventListener("DOMContentLoaded", function() {
   getBrowserPrefix();
-  populateHeader();
+  populateFooter();
   getEls();
   createEventHandlers();
   startAnimation();
 });
+
+// Luigi: All aforementioned functions begin here ==============================================================
+
+//
+// Older versions of major browsers (like Android) still use prefixes. So we figure out what that prefix is
+// and use it.
+//
+function getBrowserPrefix() {
+  // Ghetto per-browser prefixing
+  browserPrefix = getPrefix(); // could be empty string, which is fine
+  styleText = styleText.map(function(text) {
+    return text.replace(/-webkit-/g, browserPrefix);
+  });
+}
+
+//
+// Create links in header (now footer).
+// Luigi: ...Let's make it definitely a footer
+//
+function populateFooter() {
+  let footer = document.getElementById('footer');
+  footer.innerHTML = footerHTML;
+}
+
+//
+// Put els into the module scope.
+//
+function getEls() {
+  // We're cheating a bit on styles.
+  let preStyleEl = document.createElement('style');
+  preStyleEl.textContent = preStyles;
+  document.head.insertBefore(preStyleEl, document.getElementsByTagName('style')[0]);
+
+  // El refs
+  style = document.getElementById('style-tag');
+  styleEl = document.getElementById('style-text');
+  workEl = document.getElementById('work-text');
+  pgpEl = document.getElementById('pgp-text');
+  skipAnimationEl = document.getElementById('skip-animation');
+  pauseEl = document.getElementById('pause-resume');
+}
+
+//
+// Create basic event handlers for user input.
+//
+function createEventHandlers() {
+  // Mirror user edits back to the style element.
+  styleEl.addEventListener('input', function() {
+    style.textContent = styleEl.textContent;
+  });
+
+  // Skip anim on click to skipAnimation
+  skipAnimationEl.addEventListener('click', function(e) {
+    e.preventDefault();
+    animationSkipped = true;
+  });
+
+  pauseEl.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (paused) {
+      pauseEl.textContent = "Pause ||";
+      paused = false;
+    } else {
+      pauseEl.textContent = "Resume >>";
+      paused = true;
+    }
+  });
+}
 
 async function startAnimation() {
   try {
@@ -47,41 +125,7 @@ async function startAnimation() {
     }
   }
 }
-
-// Skips all the animations.
-async function surprisinglyShortAttentionSpan() {
-  if (done) return;
-  done = true;
-  pgpEl.innerHTML = pgpText;
-  let txt = styleText.join('\n');
-
-  // The work-text animations are rough
-  style.textContent = "#work-text * { " + browserPrefix + "transition: none; }";
-  style.textContent += txt;
-  let styleHTML = "";
-  for(let i = 0; i < txt.length; i++) {
-     styleHTML = handleChar(styleHTML, txt[i]);
-  }
-  styleEl.innerHTML = styleHTML;
-  createWorkBox();
-
-  // There's a bit of a scroll problem with this thing
-  let start = Date.now();
-  while(Date.now() - 1000 > start) {
-    workEl.scrollTop = Infinity;
-    styleEl.scrollTop = pgpEl.scrollTop = Infinity;
-    await Promise.delay(16);
-  }
-}
-
-
-/**
- * Helpers
- */
-
-let endOfSentence = /[\.\?\!]\s$/;
-let comma = /\D[\,]\s$/;
-let endOfBlock = /[^\/]\n\n$/;
+// Luigi: All aforementioned functions end here ==============================================================
 
 async function writeTo(el, message, index, interval, mirrorToStyle, charsPerInterval){
   if (animationSkipped) {
@@ -119,71 +163,6 @@ async function writeTo(el, message, index, interval, mirrorToStyle, charsPerInte
 }
 
 //
-// Older versions of major browsers (like Android) still use prefixes. So we figure out what that prefix is
-// and use it.
-//
-function getBrowserPrefix() {
-  // Ghetto per-browser prefixing
-  browserPrefix = getPrefix(); // could be empty string, which is fine
-  styleText = styleText.map(function(text) {
-    return text.replace(/-webkit-/g, browserPrefix);
-  });
-}
-
-//
-// Put els into the module scope.
-//
-function getEls() {
-  // We're cheating a bit on styles.
-  let preStyleEl = document.createElement('style');
-  preStyleEl.textContent = preStyles;
-  document.head.insertBefore(preStyleEl, document.getElementsByTagName('style')[0]);
-
-  // El refs
-  style = document.getElementById('style-tag');
-  styleEl = document.getElementById('style-text');
-  workEl = document.getElementById('work-text');
-  pgpEl = document.getElementById('pgp-text');
-  skipAnimationEl = document.getElementById('skip-animation');
-  pauseEl = document.getElementById('pause-resume');
-}
-
-//
-// Create links in header (now footer).
-//
-function populateHeader() {
-  let header = document.getElementById('header');
-  header.innerHTML = headerHTML;
-}
-
-//
-// Create basic event handlers for user input.
-//
-function createEventHandlers() {
-  // Mirror user edits back to the style element.
-  styleEl.addEventListener('input', function() {
-    style.textContent = styleEl.textContent;
-  });
-
-  // Skip anim on click to skipAnimation
-  skipAnimationEl.addEventListener('click', function(e) {
-    e.preventDefault();
-    animationSkipped = true;
-  });
-
-  pauseEl.addEventListener('click', function(e) {
-    e.preventDefault();
-    if (paused) {
-      pauseEl.textContent = "Pause ||";
-      paused = false;
-    } else {
-      pauseEl.textContent = "Resume >>";
-      paused = true;
-    }
-  });
-}
-
-//
 // Fire a listener when scrolling the 'work' box.
 //
 function createWorkBox() {
@@ -214,4 +193,30 @@ function createWorkBox() {
     // Scroll. If we've flipped, flip the scroll direction.
     workEl.scrollTop += (dy * (flipped ? -1 : 1));
   }, true);
+}
+
+// Skips all the animations.
+async function surprisinglyShortAttentionSpan() {
+  if (done) return;
+  done = true;
+  pgpEl.innerHTML = pgpText;
+  let txt = styleText.join('\n');
+
+  // The work-text animations are rough
+  style.textContent = "#work-text * { " + browserPrefix + "transition: none; }";
+  style.textContent += txt;
+  let styleHTML = "";
+  for(let i = 0; i < txt.length; i++) {
+     styleHTML = handleChar(styleHTML, txt[i]);
+  }
+  styleEl.innerHTML = styleHTML;
+  createWorkBox();
+
+  // There's a bit of a scroll problem with this thing
+  let start = Date.now();
+  while(Date.now() - 1000 > start) {
+    workEl.scrollTop = Infinity;
+    styleEl.scrollTop = pgpEl.scrollTop = Infinity;
+    await Promise.delay(16);
+  }
 }
